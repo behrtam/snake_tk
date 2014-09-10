2934fa1db434ee04a55e4277bb9257ffd8af649d
behrtam <behr.tam@gmail.com>
2014-09-10 07:51:10 +0200

    added support for key changeable directions

--- snake.py
+++ snake.py
@@ -10,19 +10,52 @@ Sept. 2014 @ Vancouver, CA
 import sys
 import tkinter as tk
 
+from enum import Enum, unique
+
 
 _WIDTH = 800
 _HEIGHT = 600
 
+@unique
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
 
 class SnakeCanvas(tk.Canvas):
-    ''' The snake game it self. '''
+    ''' The snake game itself. '''
     def __init__(self, master):
         tk.Canvas.__init__(self, width=_WIDTH, height=_HEIGHT,
             background='dark gray', highlightthickness=0)
 
-        self.master = master 
+        self.master = master
+        self.setup()
         self.pack()
+
+    def setup(self):
+        self.direction = Direction.LEFT
+
+        self.bind_all("<Key>", self.onKeyEvent)
+
+    def onKeyEvent(self, event):
+        key = event.keysym
+
+        if key == 'Left' and self.direction is not Direction.RIGHT:
+            self.direction = Direction.LEFT
+            print(key)
+
+        if key == 'Right' and self.direction is not Direction.LEFT:
+            self.direction = Direction.RIGHT
+            print(key)
+
+        if key == 'Up' and self.direction is not Direction.DOWN:
+            self.direction = Direction.UP
+            print(key)
+
+        if key == 'Down' and self.direction is not Direction.UP:
+            self.direction = Direction.DOWN
+            print(key)
 
 
 class SnakeFrame(tk.Frame):
